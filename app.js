18dfd35e3d62ed000cd45e582c821c6e686879a2
vikashrao12 <vikashraw12gmail.com>
2025-05-29 22:20:46 +0530

    port

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.post('/run', (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log('Server started at http://localhost:3001');
+app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+  console.log(`Server started at http://localhost:${process.env.PORT || 3001}`);
 });
+
